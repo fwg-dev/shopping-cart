cd0157b0ea18cebf7407396952801508dd29fc97
fwg-dev <fwgicheh@gmail.com>
2021-10-10 20:52:17 -0700

    import Products and Navbar components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import React, { useState } from 'react';
+//import components 
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+
 
 function App() {
+  const [products, setProducts] = useState(["shoes", "teabags", "laptops"])
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header >
         Welcome to the shopping cart
       </header>
+      <Navbar />
+      <Products products={products} />
     </div>
   );
 }
