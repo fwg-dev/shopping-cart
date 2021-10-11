b91a061203ef7d819df510f4815fbefac847b98e
fwg-dev <fwgicheh@gmail.com>
2021-10-11 07:08:08 -0700

    import navbar icons from material ui. Impleted Link to the caqrt navigation.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,27 @@ import { Link } from 'react-router-dom';
 import Cart from './Cart.js';
 import Products from './Products.js';
 
+//imports from material ui 
+import {AppBar, Toolbar, IconButton,  Typography,Button} from '@mui/material';
+
 
 function Navbar() {
   return (
-    <div>
-      <p>Hello from Navbar</p>
-      <ul>
-        <li>
-          <Link to="/"> Products</Link>
-        </li>
-        <li>
-          <Link to="/Cart"> Cart</Link>
-        </li>
-      </ul>
-    </div>
+    <div className="navigation"> 
+         <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {/* <Link to="/products"> Products</Link> */}
+          </Typography>
+          <Link to="/Cart"> 
+          <Button color="inherit">
+           Cart
+        </Button>
+        </Link>
+        </Toolbar>
+      </AppBar>
+
+  </div>
   )
 }
 
