a6e476dcbaa4274e159331d4bf3af78d6736f3db
fwg-dev <fwgicheh@gmail.com>
2021-10-11 07:06:50 -0700

    created functional compoment.

---
+++ src/components/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Cart() {
+  return (
+    <div>
+      <h1>Shopping Cart</h1>
+
+      {/* //items in the bag? */}
+      
+    </div>
+  )
+}
+
+export default Cart
