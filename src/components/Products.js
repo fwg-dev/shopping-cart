4598688c0a12ad4fdff0d6301ecd427138d56bbb
fwg-dev <fwgicheh@gmail.com>
2021-10-11 07:08:47 -0700

    mapped products from state.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Product from './Product';
 
-function Products() {
+function Products({products}) {
   return (
     <div>
       <p>Hello from Products</p>
-      {/* <Product /> */}
+      <ul>
+      {products.map((product) => (
+        <li>{product}</li>
+      ))}
+
+      </ul>
     </div>
   )
 }
