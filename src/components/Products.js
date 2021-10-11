b0b2bcba5a67717c8e16646273f932c881670f58
fwg-dev <fwgicheh@gmail.com>
2021-10-10 20:55:35 -0700

    import product component.

---
+++ src/components/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Product from './Product';
+
+function Products() {
+  return (
+    <div>
+      <p>Hello from Products</p>
+      {/* <Product /> */}
+    </div>
+  )
+}
+
+export default Products
