bc92fad9bec412417f0d8d033e6906d0a54427d4
fwg-dev <fwgicheh@gmail.com>
2021-10-12 15:18:29 -0700

    import Provider from react-redux and wrap App to it

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; 
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const store = createStore()
+
 ReactDOM.render(
+
+  // <Provider >
 
   <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode >,
   </BrowserRouter >,
+  // </Provider>,
 
   document.getElementById('root')
 );
