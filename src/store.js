6e7817db4957dcbaa72343c038eb88b3baf1f631
fwg-dev <fwgicheh@gmail.com>
2021-10-13 16:26:33 -0700

    set up our reducer store with all redux dependecies.

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
 import { createStore, applyMiddleware } from 'redux'; 
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'; 
+
+
+
+const initialState = {};
+const middleware = [thunk]; 
 
 const store = createStore(
-  reducer,
+  rootReducer,
+  initialState,
   composeWithDevTools(
     applyMiddleware(...middleware)
     // other store enhancers if any
   )
 );
+
+export default store; 
