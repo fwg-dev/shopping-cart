1445a5b3475fdbbb454cccd3baeb8c041fbbf546
fwg-dev <fwgicheh@gmail.com>
2021-10-13 16:34:26 -0700

    set up navigation routes for products and cart.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import React, { useState } from 'react';
 //import components 
 import Navbar from './components/Navbar';
 import Products from './components/Products';
@@ -16,14 +15,10 @@ function App() {
     <div className="App">
       <Navbar />
       <Switch >
-      <Route exact path ="/cart" >
-        <Cart/>
+      <Route exact path ="/products" component={Products} >
       </Route>
-
       </Switch>
-        
-        {/* <h1>Welcome to the shopping cart</h1> */}
-        <Products  />
+      <Route exact path ="/cart" component={Cart} />   
     </div>
   );
 }
